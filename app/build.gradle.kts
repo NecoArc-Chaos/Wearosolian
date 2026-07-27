891632943afaa326313afffd19f7b637b499a68c
Neco-ArcChaos <acceleratoor0123@gmail.com>
2026-07-27 15:08:14 +0000

    chore: bump version to 0.3.0beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.solsynth.solian"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.3"
+        versionCode = 2
+        versionName = "0.3.0beta"
     }
 
     signingConfigs {
